5293d95d093d52c2f7ac752dbb8070a9a5d81736
belokon.e <belokon.e@drofa.ru>
2017-11-16 17:56:36 +0300

    Создание дерева из выделенных блоков текста

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 window.onload = function () {
 	"use strict";
 
-	document.querySelector("button").onclick = function() {
-	    var selection = window.getSelection();
-	    document.getElementById("result").innerHTML = selection;
+	document.ondragstart = prohibit;
+
+	// Запрет на перетаскивание текста в области
+	function prohibit() {
+			return false; 
+		}
+
+	document.querySelector("#area").onclick = function() {
+		var p = document.createElement("p");
+	    var text = window.getSelection();
+		document.getElementById("result").appendChild(p).innerHTML = text;
 	}
 }
