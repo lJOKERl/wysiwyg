00a5b04754ec46394cc18db6059e84d264af1d59
belokon.e <belokon.e@drofa.ru>
2017-11-17 11:27:04 +0300

    Добавлены выовд и форматирование текста

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 window.onload = function () {
 	"use strict";
 
-	document.ondragstart = prohibit;
+	editorText.document.designMode = "On";
 
-	// Запрет на перетаскивание текста в области
-	function prohibit() {
-			return false; 
-		}
+	var bold = document.querySelector("#bold"),
+		italic = document.querySelector("#italic"),
+		p = document.createElement("p"),
+		title = document.createElement("h1"),
+		header = document.querySelector("#header"),
+		preview = document.querySelector("#preview"),
+		result = document.querySelector("#result"),
+		textEditor = document.querySelector("#editorText"),
+		underline = document.querySelector("#underline");
 
-	document.querySelector("#preview").onclick = function() {
-		var p = document.createElement("p");
-	    var text = window.getSelection();
-		document.getElementById("result").appendChild(p).innerHTML = text;
+	function execCmd (command) {
+		editorText.document.execCommand(command, false, null);
+	}
+
+	bold.onclick = function () {
+		execCmd("bold");
+	}
+
+	italic.onclick = function () {
+		execCmd("italic");
+	}
+
+	underline.onclick = function () {
+		execCmd("underline");
+	}
+
+	preview.onclick = function () {
+		result.appendChild(title).innerHTML = header.value;
+		result.appendChild(p).innerHTML = textEditor.contentWindow.document.body.innerHTML;
 	}
 }
