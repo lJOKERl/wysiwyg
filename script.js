093df61ecc3511e2807f33637433c0655008afdb
belokon.e <belokon.e@drofa.ru>
2017-11-17 13:32:09 +0300

    Добавлены функции почти всем кнопкам

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-window.onload = function () {
-	"use strict";
+"use strict";
 
+function desMod() {
 	editorText.document.designMode = "On";
+}
 
-	var bold = document.querySelector("#bold"),
-		italic = document.querySelector("#italic"),
-		p = document.createElement("p"),
-		title = document.createElement("h1"),
-		header = document.querySelector("#header"),
-		preview = document.querySelector("#preview"),
-		result = document.querySelector("#result"),
-		textEditor = document.querySelector("#editorText"),
-		underline = document.querySelector("#underline");
+var bold = document.querySelector("#bold"),
+	italic = document.querySelector("#italic"),
+	p = document.createElement("p"),
+	title = document.createElement("h1"),
+	header = document.querySelector("#header"),
+	body = document.getElementsByTagName('body'),
+	result = document.querySelector("#result"),
+	textEditor = document.querySelector("#editorText"),
+	underline = document.querySelector("#underline");
 
-	function execCmd (command) {
-		editorText.document.execCommand(command, false, null);
-	}
+function execCmd (command) {
+	editorText.document.execCommand(command, false, null);
+}
 
-	bold.onclick = function () {
-		execCmd("bold");
-	}
-
-	italic.onclick = function () {
-		execCmd("italic");
-	}
-
-	underline.onclick = function () {
-		execCmd("underline");
-	}
-
-	preview.onclick = function () {
+function getResult () {
+	if (header.value) {
 		result.appendChild(title).innerHTML = header.value;
-		result.appendChild(p).innerHTML = textEditor.contentWindow.document.body.innerHTML;
+	} else {
+		result.appendChild(title).innerHTML = "Заголовок"
+	}
+	result.appendChild(p).innerHTML = textEditor.contentWindow.document.body.innerHTML;
+}
+
+function execCmdWithArg (command, arg) {
+	if (arg !== 0) {
+		editorText.document.execCommand(command, false, arg);
+		console.log(1)
+	} else {
+		return
+	}
+}
+
+var textMod = true;
+var editMode = false;
+
+function toggleCode() {
+	if (textMod) {
+		textEditor.
 	}
 }
